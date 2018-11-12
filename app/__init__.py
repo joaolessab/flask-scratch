a60f2e683deb7369d92ff8497c23ca73a922032f
João Vitor Lessa <joaovitorlessa@gmail.com>
2018-11-11 23:28:22 -0200

    Migration lib added

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///storage.db'
 
 db = SQLAlchemy(app)
+
+# Migrate takes care of my migrations #
+# Manager takes care of my commands #
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 # Normal to import after app declaration #
 from app.controllers import default

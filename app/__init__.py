89d8d2065bda360b1d08af69288be6243b581d8c
João Vitor Lessa <joaovitorlessa@gmail.com>
2018-11-11 22:55:03 -0200

    Models created

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///storage.db'
+
+db = SQLAlchemy(app)
 
 # Normal to import after app declaration #
 from app.controllers import default

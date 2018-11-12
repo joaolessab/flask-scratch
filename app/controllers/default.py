3e289077ff457b7f434cf4dcd81cb7ad7a9eafaf
João Vitor Lessa <joaovitorlessa@gmail.com>
2018-11-11 23:54:47 -0200

    Routes

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,22 @@
 # Importing app from app root folder
 from app import app
 
+# http://127.0.0.1:5000/
 @app.route('/')
+@app.route('/index')
 def index():
     return "Running the API"
+
+# http://127.0.0.1:5000/test/joao
+@app.route('/test', defaults = {'name': None})
+@app.route('/test/<name>')
+def test(name):
+    if name:
+        return "Hello, %s!" % name
+    else:
+        return "Hello unknown user!"
+
+# http://127.0.0.1:5000/test/joao
+@app.route('/test', methods = ['GET'])
+def getExample():
+    return "Get request"
